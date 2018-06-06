ea06d9be695b5a69062e514ddee8d160dbfbb682
Katistic <33645731+Katistic@users.noreply.github.com>
2018-06-06 01:01:30 +0000

    Update Shalltear.py

--- Bots/Shalltear/Shalltear.py
+++ Bots/Shalltear/Shalltear.py
@@ -1,2 +1,25 @@
 import asyncio
 import discord
+
+Commands = {}
+
+class _createCommand:
+    def __init__(self, Call, Description, Function, Perms):
+        self.Call = Call
+        self.Description = Description
+        self.Function = Function
+        self.Perms = Perms
+    
+    async def Do(self, Bot, Msg):
+        if self.PermCheck(Msg):
+            await self.Function(Bot, Msg)
+    
+    def PermCheck(self, Msg):
+        Perms = Msg.author.server_permissions
+        
+        if Perms.administrator or Perms >= self.Perms:
+            return True
+        return False
+
+def createCommand(Call, Description, Function, Perms):
+    return __createCommand(Call, Description, Function, Perms)
