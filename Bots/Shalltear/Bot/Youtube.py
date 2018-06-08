88ea33e5b64de770730962f03b709264d00963d4
Katistic <33645731+Katistic@users.noreply.github.com>
2018-06-08 10:04:45 +1000

    Update Youtube.py

--- Bots/Shalltear/Bot/Youtube.py
+++ Bots/Shalltear/Bot/Youtube.py
@@ -1,1 +1,17 @@
+from googleapiclient.descovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
 
+
+def search_list(query, mResults = 1, t = "video"):
+    vList = _search_list(client,
+                         part = 'snippet',
+                         maxResults = mResults,
+                         q = query,
+                         type = t)
+    
+    return vList
+
+def _search_list(**kwargs):
+    response = client.search().list(**kwargs).execute()
+    
+    
