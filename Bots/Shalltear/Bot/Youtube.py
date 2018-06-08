4c12a566f5d4139aa9be6d0534273ec0eeb2f0c5
Katistic <33645731+Katistic@users.noreply.github.com>
2018-06-08 11:02:09 +1000

    Update Youtube.py

--- Bots/Shalltear/Bot/Youtube.py
+++ Bots/Shalltear/Bot/Youtube.py
@@ -1,17 +1,41 @@
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+
 from googleapiclient.descovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 
+CLIENT_SECRETS_FILE = ".\\Bot\\client_secret.json"
 
-def search_list(query, mResults = 1, t = "video"):
+SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+
+client = serv()
+
+def serv():
+    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+    credentials = flow.run_console()
+    return build(API_SERVICE_NAME, API_VERSION) #, credentials = credentials)
+
+def search_list(query, mResults = 1):
     vList = _search_list(client,
                          part = 'snippet',
                          maxResults = mResults,
                          q = query,
-                         type = t)
+                         type = "video")
     
     return vList
 
 def _search_list(**kwargs):
     response = client.search().list(**kwargs).execute()
     
+    items = []
+    for x in response['items']:
+        Id = x["id"]["videoId"]
+        Title = x["snippet"]["title"]
+        ChanId = x["snippet"]["channelId"]
+        ChanTitle = x["snippet"]["channelTitle"]
+        Thumb = x["snippet"]["thumbnails"]["default"]["url"]
     
+    return items
+
