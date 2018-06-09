70a6d96695935497d5f3d03a8f6f671264f395a0
Inga因果 <33645572+Inga74u@users.noreply.github.com>
2018-06-09 12:06:44 +1000

    Update Command.java

--- Bots/Kazuki/src/main/java/inga74u/discord/kazuki/commands/Command.java
+++ Bots/Kazuki/src/main/java/inga74u/discord/kazuki/commands/Command.java
@@ -1,1 +1,20 @@
+package inga74u.discord.kazuki.commands;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Rentention;
+import java.lang.annotation.RententionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value = ElementType.METHOD)
+@Rentention(RetentionPolicy.RUNTIME)
+
+public @interface Command {
+  String name();
+  String description() default "Description unset.";
+  ExecutorType type() default ExecutorType.ALL;
+  
+  // This also had CONSOLE, for commands called from the Thanatos console. We don't need that in this project
+  enum ExecutorType {
+    ALL, USER
+  }
+}
