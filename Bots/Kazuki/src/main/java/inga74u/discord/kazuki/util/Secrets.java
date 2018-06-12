39ce4565dfe464ba565ae455742a8a52d7936f6d
Inga因果 <33645572+Inga74u@users.noreply.github.com>
2018-06-12 19:44:06 +1000

    Create Secrets.java

---
+++ Bots/Kazuki/src/main/java/inga74u/discord/kazuki/util/Secrets.java
@@ -0,0 +1,13 @@
+package inga74u.discord.kazuki.util;
+
+import java.awt.*;
+
+public class Secrets {
+    public static String PREFIX = "..";
+    public static String TOKEN = "DISCORD_TOKEN_HERE";
+    public static String YTAPIKEY = "YT_API_KEY_HERE";
+    public static String RIOTAPIKEY = "RIOT_API_KEY_HERE";
+    public static String OSUAPIKEY = "OSU_API_KEY_HERE";
+    public static int MAXSONGS = 5; // For playlist
+    public static Color EMBEDCOLOR = Color.decode("0x9900ff");
+}
