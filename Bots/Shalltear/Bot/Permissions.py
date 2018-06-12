b41335bf509b8dbb2909c08c5abe8743b5bc3ab9
Katistic <33645731+Katistic@users.noreply.github.com>
2018-06-13 07:55:47 +1000

    Add a few more permissions

--- Bots/Shalltear/Bot/Permissions.py
+++ Bots/Shalltear/Bot/Permissions.py
@@ -1,10 +1,38 @@
 Default = "Default"
 Administrator = "Admin"
+Kick = "Kick"
+Ban = "Ban"
+ManageChannels = "ManageChan"
+ManageServer = "ManageServer"
+ManageMessages = "ManageMessages"
+MoveMembers = "MoveMembers"
+ManageRoles = "ManageRoles"
 
 def PermsList(Perms):
     P = []
     
     if Perms.administrator:
         P.append(Administrator)
+    
+    if Perms.kick_members:
+        P.append(Kick)
+    
+    if Perms.ban_members:
+        P.append(Ban)
+        
+    if Perms.manage_channels:
+        P.append(ManageChannels)
+    
+    if Perms.manage_server:
+        P.append(ManageServer)
+    
+    if Perms.manage_messages:
+        P.append(ManageMessages)
+    
+    if Perms.move_members:
+        P.append(MoveMembers)
+    
+    if Perms.manage_roles:
+        P.append(ManageRoles)
     
     return P
