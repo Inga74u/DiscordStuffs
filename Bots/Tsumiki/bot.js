9a98def7407afdd61430bc6048e19a68274d9f7e
Inga因果 <33645572+Inga74u@users.noreply.github.com>
2018-06-15 07:46:41 +1000

    Update bot.js

--- Bots/Tsumiki/bot.js
+++ Bots/Tsumiki/bot.js
@@ -34,13 +34,51 @@ function mimic(msg) {
     var args = msg.content.split(" ");
     if(args.length < 2) {
         msg.channel.send("Mimic requires more than 0 arguments");
-    } else {
-        args.shift();
-        msg.channel.send(args.join(" "));
+        return;
     }
+    args.shift();
+    msg.channel.send(args.join(" "));
 }
 
+function preix(msg) { // I got lazy, I'll fix it later
+    var args = msg.content.split(" ");
+    if(args.length < 2) {
+        msg.channel.send("Must have args");
+        return;
+    }
+    guilds[msg.guild.id]['prefix'] = args[1];
+    msg.channel.send("Prefix changed to: " + guilds[msg.guild.id]['prefix']);
+}
 
+function purge(msg) {
+    var args = msg.content.split(" ");
+    if(args.length < 2) {
+        msg.channel.send("Purge requires more than 0 arguments");
+        return;
+    }
+    
+    parseInt(args[1]);
+    if(args[1] instanceof int) {
+        msg.channel.send("First argument must be a number");
+        return;
+    }
+    
+    msg.channel.bulkDelete(args[1]);
+}
+
+function play(msg) {
+    var args = msg.content.split(" ");
+    if(args.length < 2) {
+        msg.channel.send("Play requires more than 0 arguments");
+        return;
+    }
+    
+    if(!args[1].indexOf("youtube.com")) {
+        msg.channel.send("1st argument must be a YouTube link");
+        return;
+    }
+    //play stuff
+}
 
 // Main bot section
 const miniwa = new discord.Client();
@@ -51,6 +89,8 @@ miniwa.on('ready', () => {
         guilds[key.toString()] = {
             'name': value.toString(),
             'prefix': "tsu."
+            'player': ,
+            'queue': []
         };
         console.log(guilds[key.toString()]['name'] + ": " + key.toString()); // Just to test that it's working
     });
@@ -68,15 +108,8 @@ miniwa.on('message', msg => {
 });
 
 createCommand("mimic", "Mimics what you tell me too. (Usage: mimic [text to mimic])", mimic);
-createCommand("prefix", "Changes the prefix. (Usage: prefix [new prefix])", function(msg) { // I got lazy, I'll fix it later
-    var args = msg.content.split(" ");
-    if(args.length < 2) {
-        msg.channel.send("Must have args");
-        return;
-    }
-    guilds[msg.guild.id]['prefix'] = args[1];
-    msg.channel.send("Prefix changed to: " + guilds[msg.guild.id]['prefix']);
-});
+createCommand("prefix", "Changes the prefix. (Usage: prefix [new prefix])", prefix);
+createCommand("purge", "Deletes an amount of messages from a TextChannel. (Usage: purge [number of messages to purge])", purge);
 
 /* This is also possible:
 
