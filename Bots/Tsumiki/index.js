daf6a8c6f5246a023695b83dae3509988be91a50
Inga因果 <33645572+Inga74u@users.noreply.github.com>
2018-06-14 12:08:55 +1000

    Create index.js

---
+++ Bots/Tsumiki/index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const Secrets = require('Secrets.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+});
+
+client.login(Secrets.TOKEN);
